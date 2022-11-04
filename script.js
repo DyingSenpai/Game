0964fb7aea9370a1623d54a93fc6c6483b2bd2fd
DyingSenpai <ratulird@gmail.com>
2022-11-04 23:13:56 +0300

    Added score per sec function

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 'use strict'
 let elemHit = document.getElementById("hit");
 let elemScore = document.getElementById("score");
+let elemBuyWeapon = document.getElementById("buyWeapon");
+let elemScorePersec = document.getElementById("scorePerSec");
+
 let score = parseInt(elemScore.innerHTML);
+let scorePerSec = parseInt(elemScorePersec.innerHTML);
+
+let updateScore = function(score) {
+    elemScore.innerHTML = score;
+}
+
+let updateScorePerSec = function(scorePerSec) {
+    elemScorePersec.innerHTML = scorePerSec;
+}
 
 //Clicker
 elemHit.onclick = function() {
-    score = score + 1;
-    elemScore.innerHTML = score;
+    score += 1;
+    updateScore(score);
+}
+
+elemBuyWeapon.onclick = function() {
+    if(score < 20) {
+        alert("not enough gold");
+    }
+    else {
+        score -= 20;
+        scorePerSec += 1;
+        updateScore(score);
+        updateScorePerSec(scorePerSec);
+        hitPerSec(scorePerSec);
+    }
+}
+
+let hitPerSec = function(scorePerSec) {
+    setInterval(() => {
+        score += scorePerSec;
+        updateScore(score);
+    }, 1000);
 }
